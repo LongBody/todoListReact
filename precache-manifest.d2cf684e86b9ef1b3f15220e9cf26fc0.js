33cca4e89950dec72e8cd92e2ae54a847eb51c3b
LongBody <54003924+LongBody@users.noreply.github.com>
2020-04-22 13:04:28 +0700

    Updates

---
+++ precache-manifest.d2cf684e86b9ef1b3f15220e9cf26fc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "564e4b3fcd1ee090f4770de0770a1dfc",
+    "url": "/todoListReact/index.html"
+  },
+  {
+    "revision": "0f5defbfa5c42d57b66f",
+    "url": "/todoListReact/static/css/main.8f5a2eb7.chunk.css"
+  },
+  {
+    "revision": "a4a09756a08a05e29342",
+    "url": "/todoListReact/static/js/2.7025ff2e.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/todoListReact/static/js/2.7025ff2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f5defbfa5c42d57b66f",
+    "url": "/todoListReact/static/js/main.61a6fab2.chunk.js"
+  },
+  {
+    "revision": "3234f0f089bc03e0b2ee",
+    "url": "/todoListReact/static/js/runtime-main.ce9d225b.js"
+  }
+]);
